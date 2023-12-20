78818ac93f6796f5bc86f45ab513224565cb6259
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-20 13:30:14 +0100

    init Main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MainPage = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <h1>De wereld van het lassen</h1>
+    </div>
+  );
 };
 
 export default MainPage;
