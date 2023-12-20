5beba89b77fd29800f282efc2d9ba81d02b9690c
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-20 12:25:26 +0100

    set router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import MainPage from "./MainPage";
+import Materials from "./Materials";
+
 function App() {
-  return <div><h1>
-  De wereld van het lassen</h1></div>;
+  return (
+    <div>
+      <h1>De wereld van het lassen</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/materials" element={<Materials />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
