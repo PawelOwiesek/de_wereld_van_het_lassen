523655229ff81a3504919163cd45cb0aeb4da5b2
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-21 16:54:04 +0100

    styles for background Image

--- src/pages/main/styled.js
+++ src/pages/main/styled.js
@@ -4,3 +4,10 @@ export const Header = styled.h1`
   text-align: center;
   color: #008080;
 `;
+export const TopImage = styled.p`
+  background-image: url("https://i.postimg.cc/65nfzmp9/download-1.jpg");
+  height: 550px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
