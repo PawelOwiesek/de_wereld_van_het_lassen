9409563ae3663ef254a3e51b489e63770218435d
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-21 16:08:23 +0100

    updated link path

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -1,10 +1,10 @@
-import { NavBar, NavItem } from "./styles";
+import { NavBar, NavItem } from "./styled";
 
 const Navbar = () => {
   return (
     <NavBar>
       <NavItem to="/">Main Page</NavItem>
-      <NavItem to="/materials">Materials</NavItem>
+      <NavItem to="/Constructiestaal">Constructiestaal</NavItem>
     </NavBar>
   );
 };
