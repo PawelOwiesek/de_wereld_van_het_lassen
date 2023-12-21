c22a910a0b3198b2241e2e4a0086412afb8e9931
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-21 16:04:43 +0100

    add box-model and font-family

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0 auto;
-    padding: 0;
-    max-width:1400px;
-    background-color: #f0f0f0;
-    color: #1f1f1f;
-  }`;
+export const GlobalStyle = createGlobalStyle` html {
+  box-sizing: border-box;
+};
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+};
+
+body {
+  margin: 0 auto;
+  padding: 0;
+  max-width: 1400px;
+  background-color: #fff;
+  color: #1f1f1f;
+  font-family: 'Montserrat', sans-serif;
+}`;
