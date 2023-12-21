051142594df63099019e6118cc122af1b44cb637
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-21 16:06:35 +0100

    created component Construciestaal with content

--- src/Materials.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Materials = () => {
-  return <div>S266 stal</div>;
-};
-
-export default Materials;
---
+++ src/materials/Constructiestaal.js
@@ -0,0 +1,299 @@
+import {
+  Container,
+  Content,
+  ContentTitle,
+  Divider,
+  Header,
+  Image,
+  Inleiding,
+  InleidingParagraph,
+  List,
+  ListItem,
+  Paragraph,
+  SubHeader,
+  SubParagraph,
+} from "./styled";
+
+const Constructiestaal = () => {
+  return (
+    <>
+      <Header>
+        Verschillende Soorten Constructiestaal: Toepassingen en Eigenschappen
+      </Header>
+      <main>
+        <section>
+          <SubHeader> Soorten en Typen Staal</SubHeader>
+          <Inleiding>
+            <InleidingParagraph>
+              <strong>
+                Staal is ongetwijfeld een van de meest populaire en
+                veelgebruikte materialen ter wereld. Het wordt gebruikt in de
+                bouw, automotive, gastronomie en zelfs in de medische sector.
+                Echter, niet alle staalsoorten zijn gelijk, en niet elk type is
+                geschikt voor het bouwen van metalen constructies of voor
+                gebruik in de keuken. Er zijn al vele soorten van dit materiaal
+                gecreëerd, en elk ervan dient een ander doel. In dit artikel
+                leer je meer over de soorten en toepassingen van staal.
+              </strong>{" "}
+            </InleidingParagraph>
+            <p>
+              <Image
+                src="https://i.postimg.cc/vHBDsmkT/download.jpg"
+                alt="plaat metaal"
+              />
+            </p>
+          </Inleiding>
+          <Inleiding>
+            <p>
+              <Image
+                src="https://i.postimg.cc/KcCbjr86/Huta-Stali-Arcelor-Mittal.jpg"
+                alt=""
+              />
+            </p>
+            <div>
+              {" "}
+              <SubHeader>Wat is staal en hoe wordt het gemaakt?</SubHeader>
+              <Paragraph>
+                Staal is een legering van ijzer en koolstof en behoort tot een
+                van de meest populaire en veelgebruikte materialen ter wereld.
+                Ondanks zijn populariteit weten maar weinig mensen echt wat
+                staal onderscheidt van andere metalen. Allereerst moet worden
+                opgemerkt dat het een legering is van ijzer en koolstof. Volgens
+                de normen moet het eerste bestanddeel het grootste deel van de
+                mix uitmaken, terwijl het tweede niet meer dan 2,11% mag
+                bedragen. Als er meer koolstof aanwezig is, spreken we al van
+                gietijzer. Bovendien bevatten staalsoorten ook andere metalen,
+                elementen en niet-metalen insluitsels die van invloed kunnen
+                zijn op hun eigenschappen. Meer hierover kom je te weten in het
+                verdere verloop van het artikel. Voor de productie van de
+                legering worden meestal twee methoden gebruikt: de
+                hoogovenmethode en de elektrische boogovenmethode. Bij de eerste
+                methode wordt ijzererts, koolstof en additieven gebruikt om
+                zogenaamd "ruw ijzer" te produceren. Vervolgens wordt dit
+                onderworpen aan ontgassing, een selectieve oxidatie die
+                ongewenste bestanddelen, waaronder slak, verwijdert. De
+                resulterende mix wordt gegoten en bijvoorbeeld gewalst om
+                producten voor de metaalindustrie te verkrijgen, zoals staven,
+                platen of buizen. De methode met de elektrische boogoven is
+                gebaseerd op het gebruik van gerecycled staalschroot. Dit
+                schroot wordt gesmolten met behulp van elektrische energie die
+                stroomt tussen het geladen materiaal en de elektroden. Omdat
+                staal gekenmerkt wordt door een hoge duurzaamheid, is de
+                hoeveelheid recyclebaar schroot die kan worden verwerkt in
+                boogovens relatief klein. Dit resulteert in een frequenter
+                gebruik van de hoogovenmethode.
+              </Paragraph>
+            </div>
+          </Inleiding>
+        </section>
+        <Content>
+          <SubHeader>
+            Mechanische en technologische eigenschappen van staal
+          </SubHeader>
+          <Container>
+            <Paragraph>
+              Zoals elk materiaal heeft staal verschillende parameters die
+              belangrijk zijn voor een specifieke branche. In dit hoofdstuk
+              richten we ons op kenmerken die relevant zijn voor de bouw, omdat
+              hierin de belangrijkste punten worden behandeld. Hier zijn ze:
+            </Paragraph>
+            <List>
+              <ListItem>
+                Elasticiteit – Het vermogen van het materiaal om zijn
+                oorspronkelijke vorm terug te krijgen nadat er een kracht op
+                heeft gewerkt die vervorming veroorzaakt. Vaak wordt ook de
+                elasticiteitsgrenswaarde vermeld. Na overschrijding hiervan
+                ondergaat het materiaal blijvende vervorming en keert het niet
+                terug naar de oorspronkelijke vorm.
+              </ListItem>
+              <ListItem>
+                {" "}
+                Rekweerstand – Het niveau van spanning veroorzaakt door een
+                trekkende kracht.
+              </ListItem>
+              <ListItem>
+                {" "}
+                Plasticiteit – Het gedrag van vervormde vorm als gevolg van
+                spanning. Net als bij elasticiteit is er hier ook het concept
+                van de plastische grens. Dit is het punt waarop onomkeerbare
+                plastische vervormingen optreden.
+              </ListItem>{" "}
+              <ListItem>
+                Rekbaarheid – Het gedrag van de staaleigenschappen tijdens
+                persen, buigen, rechtzetten en andere soorten bewerking.
+              </ListItem>
+              <ListItem>
+                Slaagsterkte – De weerstand tegen dynamische belastingen.
+              </ListItem>
+              <ListItem>
+                {" "}
+                Hardheid – De kracht waarmee staal weerstand biedt bij het
+                indrukken door harder materiaal.{" "}
+              </ListItem>
+              <ListItem>
+                Lasbaarheid – Het vermogen om duurzame verbindingen te creëren
+                door middel van lassen. Afhankelijk van het type legering kunnen
+                methoden zoals MAG, MIG, MIG/MAG en TIG worden toegepast.
+              </ListItem>
+            </List>
+            <Image
+              width="450"
+              height="400"
+              src="https://i.postimg.cc/90VJhzy0/images.jpg"
+              alt="lasapparaat"
+            />
+          </Container>
+        </Content>
+        <Content>
+          <div>
+            <SubHeader>Welke staalsoorten zijn er?</SubHeader>
+            <Paragraph>
+              Staalclassificatie komt voort uit verschillende factoren, waarvan
+              de chemische samenstelling een van de belangrijkste is. Op basis
+              hiervan worden twee hoofdtypen onderscheiden: ongelegeerd staal en
+              gelegeerd staal. Wat hen onderscheidt, is de aanwezigheid van
+              toevoegingen die worden gebruikt om eigenschappen te verbeteren of
+              nieuwe eigenschappen te verkrijgen. Een andere veelgebruikte
+              indeling houdt rekening met het gebruik van het materiaal. Hier
+              worden onder andere constructiestaal, gereedschapsstaal en
+              speciaal staal, waaronder corrosiebestendig staal, genoemd.
+            </Paragraph>
+          </div>
+          <ContentTitle>Ongelegeerde staalsoorten</ContentTitle>
+          <Paragraph>
+            Indeling van ongelegeerd staal is onder andere gebaseerd op het
+            koolstofgehalte, wat wordt aangeduid als koolstofstaal. De invloed
+            van dit element is belangrijk voor de algehele hardheid en
+            plasticiteit van het materiaal, daarom wordt het in verschillende
+            hoeveelheden gebruikt, afhankelijk van het beoogde effect. Hieronder
+            staan de soorten koolstofstaal: Laagkoolstofstaal: Ook bekend als
+            zacht staal, bevat tot ongeveer 0,3% koolstof. Het kenmerkt zich
+            door hoge plasticiteit, rekbaarheid en gemak van bewerking. Het
+            wordt gebruikt voor het vervaardigen van onderdelen die torderen en
+            vormgeving vereisen. Mediumkoolstofstaal: Het vormt een compromis
+            tussen sterkte en plasticiteit. Met een koolstofgehalte tussen 0,3
+            en 0,6% is het harder dan laagkoolstofstaal, maar tegelijkertijd
+            moeilijker te bewerken. Het kan worden versterkt door middel van
+            warmtebehandeling en harden, wat het geschikt maakt voor de
+            productie van auto-onderdelen. Hoogkoolstofstaal: De sterkste in
+            deze classificatie, met een hoog koolstofgehalte (tot 0,6%). Dit
+            maakt het bestand tegen snijden en buigen, waardoor het ideaal is
+            voor de productie van bijvoorbeeld veren. Het kan worden onderworpen
+            aan warmtebehandeling voor verharding, maar dit verhoogt de
+            gevoeligheid voor broosheid.
+          </Paragraph>
+        </Content>
+
+        <Content>
+          {" "}
+          <ContentTitle>Gelegeerd staal</ContentTitle>
+          <SubParagraph>
+            Het komt voor dat er behoefte is aan het gebruik van materiaal met
+            specifieke eigenschappen die gewoon staal niet heeft. Om dit te
+            bereiken, worden tijdens de productie elementen toegevoegd die
+            nieuwe kenmerken geven. Dit is precies waar gelegeerd staal om de
+            hoek komt kijken. Volgens de Poolse normen moet de inhoud van een
+            toegevoegd element gelijk zijn aan of groter zijn dan die vermeld in
+            de tabel met toegestane massaconcentraties. Wat zijn de voordelen
+            van het gebruik van legeringselementen?
+          </SubParagraph>
+          <List>
+            <ListItem>Verhoogt de hardbaarheid van staal.</ListItem>
+            <ListItem>Men verkrijgt specifieke sterkte-eigenschappen.</ListItem>
+            <ListItem>
+              Er treden veranderingen op in de structuur van de legering.
+            </ListItem>
+            <ListItem>
+              Thermische bewerking verloopt gemakkelijker en levert betere
+              resultaten op.
+            </ListItem>
+            <ListItem>
+              Staal krijgt nieuwe chemische en fysische eigenschappen.
+            </ListItem>
+          </List>
+          <Divider />
+          <div>
+            <SubParagraph>
+              {" "}
+              Welke parameters verbeterd zullen worden met behulp van
+              legeringselementen, hangt af van het gekozen element. Elk ervan
+              geeft verschillende eigenschappen. Vaak worden meerdere
+              toevoegingen tegelijkertijd gebruikt om volledig nieuwe kenmerken
+              te verkrijgen. Hier zijn enkele van de populaire elementen die
+              worden gebruikt bij de productie van gelegeerd staal:
+            </SubParagraph>
+            <List>
+              <ListItem>
+                {" "}
+                Chroom – verbetert de sterkte, hardheid en taaiheid van het
+                materiaal. In sommige soorten, zoals gereedschapsstaal, heeft
+                het een positief effect op de hardbaarheid. In roestvrijstalen
+                legeringen zorgt het voor corrosiebestendigheid.{" "}
+              </ListItem>
+              <ListItem>
+                Nikkel – geeft vergelijkbare eigenschappen als chroom. Bovendien
+                verlaagt het de temperatuur van de brosheidsdrempel. In
+                combinatie met chroom en molybdeen verbetert het de hardbaarheid
+                van staal.
+              </ListItem>
+              <ListItem>
+                {" "}
+                Mangaan – verhoogt ook de hardheid en sterkte. Aan de andere
+                kant vermindert het de plasticiteit van de legering, wat leidt
+                tot een verhoging van de rekgrens en een versterking van de
+                slijtvastheid.
+              </ListItem>
+              <ListItem>
+                {" "}
+                Silicium – wordt toegevoegd aan het ruwe ijzer vanwege zijn
+                ontlatende eigenschappen. Het verhoogt de sterkte en hardheid
+                van staal.
+              </ListItem>
+              <ListItem>
+                Molybdeen – wordt gebruikt om de hardbaarheid van staal te
+                vergroten. Het vermindert ook de broosheid na hoog ontlaten.
+              </ListItem>{" "}
+            </List>
+          </div>
+          <Divider />
+          <div>
+            <SubParagraph>
+              Gelegeerd staal kan worden onderverdeeld op basis van de
+              hoeveelheid toevoegingen die worden gebruikt bij de productie
+              ervan. Er zijn drie soorten:
+            </SubParagraph>
+            <List>
+              <ListItem>
+                Laaggelegeerd staal - het gehalte aan het belangrijkste
+                toevoegsel is minder dan 2%, en de som van alle toevoegingen
+                overschrijdt niet 3,5%.
+              </ListItem>
+              <ListItem>
+                Middelgelegeerd staal - het gehalte aan het belangrijkste
+                toevoegsel varieert van 2 tot 8%, en de som van alle
+                toevoegingen overschrijdt niet 12%.
+              </ListItem>
+              <ListItem>
+                Hooggelegeerd staal - het gehalte aan het belangrijkste
+                toevoegsel is meer dan 8%, en de som van alle toevoegingen
+                overschrijdt niet 55%.
+              </ListItem>
+            </List>
+          </div>
+        </Content>
+      </main>
+      <footer>
+        Auteur: Pawel Owiesek ©
+        <p> [2024] Pawel Owiesek. Alle rechten voorbehouden.</p>
+        <strong>
+          De inhoud van deze website is beschermd door het auteursrecht en mag
+          niet worden gereproduceerd, verspreid, verzonden, weergegeven of
+          uitgezonden zonder schriftelijke toestemming van de auteur. Neem voor
+          eventuele vragen of toestemming contact op met [ Owiesek.P@gmail.com].
+        </strong>
+      </footer>
+    </>
+  );
+};
+
+export default Constructiestaal;
