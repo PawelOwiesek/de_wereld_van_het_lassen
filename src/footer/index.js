0debb52c5885a56080da0642ca7cfddb701ad0d2
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-22 15:03:36 +0100

    created component Footer with styles

---
+++ src/footer/index.js
@@ -0,0 +1,18 @@
+import { Container } from "./styled";
+
+const Footer = () => {
+  return (
+    <Container>
+      Auteur: Pawel Owiesek ©
+      <p> [2024] Pawel Owiesek. Alle rechten voorbehouden.</p>
+      <strong>
+        De inhoud van deze website is beschermd door het auteursrecht en mag
+        niet worden gereproduceerd, verspreid, verzonden, weergegeven of
+        uitgezonden zonder schriftelijke toestemming van de auteur. Neem voor
+        eventuele vragen of toestemming contact op met [ Owiesek.P@gmail.com].
+      </strong>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/footer/styled.js
@@ -0,0 +1,4 @@
+import { styled } from "styled-components";
+export const Container = styled.footer`
+  margin: 10px;
+`;
