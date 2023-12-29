0e7a8ce243232c440da044b3962be9a1ae91cc2f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-29 12:07:58 +0100

    add new content and styles

--- src/pages/lassen/index.js
+++ src/pages/lassen/index.js
@@ -1,79 +1,197 @@
 import Footer from "../../footer";
-import { Inleiding } from "../materiaals/styled";
-import { Header, SubHeader } from "./styled";
+import { Content, Image, Inleiding, Paragraph } from "../materiaals/styled";
+import { Header, Methoden, SubHeader } from "./styled";
 
 const Lassen = () => {
   return (
     <>
       <Header>Lassen met de MIG/MAG-methode: Alles wat je moet weten</Header>
-      <Inleiding>
-        <p>
-          Lassen met de MIG/MAG-methode (Metal Inert Gas/Metal Active Gas) is
-          een van de meest populaire technieken om metalen te verbinden,
-          gebruikt in industrie, constructie en vele andere vakgebieden. In dit
-          artikel gaan we dieper in op deze fascinerende technologie, bespreken
-          we de toepassingen, werkingsprincipes en de voordelen en uitdagingen
-          die ermee gepaard gaan.
-        </p>
-      </Inleiding>{" "}
-      <Inleiding>
-        <SubHeader> Basisprincipes van MIG/MAG</SubHeader>{" "}
-        <p>
-          De MIG/MAG-methode maakt gebruik van een elektrische boog die wordt
-          onderhouden tussen de elektrodedraad en de te verbinden metalen. Bij
-          MIG worden neutrale gassen zoals argon of helium gebruikt om de boog
-          te beschermen tegen chemische reacties met de omgeving. Bij MAG worden
-          actieve gassen gebruikt, zoals koolstofdioxide of gasmengsels, die ook
-          invloed hebben op het lasproces.
-        </p>
-      </Inleiding>{" "}
-      <Inleiding>
-        <SubHeader>Voordelen van MIG/MAG</SubHeader>
-        <p>
-          Snelheid en Efficiëntie: Lassen met MIG/MAG is aanzienlijk sneller dan
-          veel andere methoden, wat het efficiënt maakt voor massaproductie.{" "}
-          Veelzijdigheid: Deze techniek is geschikt voor het lassen van diverse
-          soorten metalen, waaronder roestvrij staal, aluminium en
-          metaallegeringen. Kwaliteit van Verbindingen: De lassen die worden
-          verkregen met MIG/MAG zijn vaak uitzonderlijk duurzaam, waardoor ze
-          ideaal zijn voor constructies die hoge sterkte vereisen.
-        </p>
-      </Inleiding>{" "}
-      <Inleiding>
-        <SubHeader>Toepassingen</SubHeader>{" "}
-        <p>
-          {" "}
-          Automobielindustrie: MIG/MAG wordt veel gebruikt voor de productie van
-          auto-carrosserieën, uitlaatpijpen en andere componenten. Metalen
-          Constructies: In de bouw- en constructie-industrie wordt MIG/MAG veel
-          toegepast voor het verbinden van structurele elementen.
-          Luchtvaartindustrie: Vanwege de precisie en duurzaamheid wordt MIG/MAG
-          gebruikt voor de productie van luchtvaartcomponenten.{" "}
-        </p>
-      </Inleiding>
-      <Inleiding>
-        {" "}
-        <SubHeader>Uitdagingen</SubHeader>{" "}
-        <p>
-          {" "}
-          Warmteafvoer: Bij het MIG/MAG-proces kan de grote hoeveelheid warmte
-          de metaalstructuur beïnvloeden, wat extra controle en koeling kan
-          vereisen. Gasverontreiniging: Vooral bij MAG kunnen actieve gassen de
-          kwaliteit van de las beïnvloeden, vooral op plaatsen met beperkte
-          ventilatie.
-        </p>{" "}
-      </Inleiding>{" "}
-      <Inleiding>
+      <main>
+        <section>
+          <Inleiding>
+            <Image
+              src="https://i.postimg.cc/VkzWjqwm/MIG-MAG-lassen-voorbeeld.jpg"
+              alt="lassen mig/mag"
+            />
+            <Paragraph>
+              Lassen met de MIG/MAG-methode (Metal Inert Gas/Metal Active Gas)
+              is een van de meest populaire technieken om metalen te verbinden,
+              gebruikt in industrie, constructie en vele andere vakgebieden. In
+              dit artikel gaan we dieper in op deze fascinerende technologie,
+              bespreken we de toepassingen, werkingsprincipes en de voordelen en
+              uitdagingen die ermee gepaard gaan.
+            </Paragraph>
+          </Inleiding>
+          <SubHeader> Basisprincipes van MIG/MAG</SubHeader>{" "}
+          <Inleiding>
+            <Paragraph>
+              De MIG/MAG-methode maakt gebruik van een elektrische boog die
+              wordt onderhouden tussen de elektrodedraad en de te verbinden
+              metalen. Bij MIG worden neutrale gassen zoals argon of helium
+              gebruikt om de boog te beschermen tegen chemische reacties met de
+              omgeving. Bij MAG worden actieve gassen gebruikt, zoals
+              koolstofdioxide of gasmengsels, die ook invloed hebben op het
+              lasproces.
+            </Paragraph>
+            <Image
+              src="https://i.postimg.cc/KzjpSdv9/mig-mag-lassen-width-1032-c-JKl-Eim.png"
+              alt="lassen mig/mag"
+            />
+          </Inleiding>
+          <SubHeader>Voordelen van MIG/MAG</SubHeader>
+          <Inleiding>
+            {" "}
+            <Image
+              src="https://i.postimg.cc/QVL00424/download-3.jpg"
+              alt="lasapparaat"
+            />
+            <Paragraph>
+              Snelheid en Efficiëntie: Lassen met MIG/MAG is aanzienlijk sneller
+              dan veel andere methoden, wat het efficiënt maakt voor
+              massaproductie. Veelzijdigheid: Deze techniek is geschikt voor het
+              lassen van diverse soorten metalen, waaronder roestvrij staal,
+              aluminium en metaallegeringen. Kwaliteit van Verbindingen: De
+              lassen die worden verkregen met MIG/MAG zijn vaak uitzonderlijk
+              duurzaam, waardoor ze ideaal zijn voor constructies die hoge
+              sterkte vereisen.
+            </Paragraph>
+            <Image
+              src="https://i.postimg.cc/nLtfBbhz/images-5.jpg"
+              alt="lasapparaat"
+            />{" "}
+            <Image
+              src="https://i.postimg.cc/ncL5DVKT/images-4.jpg"
+              alt="lasapparaat"
+            />
+          </Inleiding>{" "}
+          <SubHeader>Toepassingen</SubHeader>{" "}
+          <Inleiding>
+            <Paragraph>
+              {" "}
+              Automobielindustrie: MIG/MAG wordt veel gebruikt voor de productie
+              van auto-carrosserieën, uitlaatpijpen en andere componenten.
+              <p>
+                Metalen Constructies: In de bouw- en constructie-industrie wordt
+                MIG/MAG veel toegepast voor het verbinden van structurele
+                elementen.
+              </p>{" "}
+              <p>
+                Luchtvaartindustrie: Vanwege de precisie en duurzaamheid wordt
+                MIG/MAG gebruikt voor de productie van luchtvaartcomponenten.{" "}
+              </p>
+            </Paragraph>{" "}
+            <Image
+              src=" https://i.postimg.cc/1X51YpkN/22996377-robot-lassen-automotive-een-deel-bijeenkomst-in-auto-fabriek-ai-gegenereerd-foto.jpg"
+              alt="lasapparaat"
+            />
+          </Inleiding>
+          <SubHeader>Uitdagingen</SubHeader>{" "}
+          <Content>
+            {" "}
+            <Paragraph>
+              {" "}
+              Warmteafvoer: Bij het MIG/MAG-proces kan de grote hoeveelheid
+              warmte de metaalstructuur beïnvloeden, wat extra controle en
+              koeling kan vereisen. Gasverontreiniging: Vooral bij MAG kunnen
+              actieve gassen de kwaliteit van de las beïnvloeden, vooral op
+              plaatsen met beperkte ventilatie.
+            </Paragraph>{" "}
+          </Content>{" "}
+        </section>
+
+        <section>
+          <SubHeader>Lassen met methode 136: Alles wat je moet weten</SubHeader>{" "}
+          <Methoden>
+            <Paragraph>
+              <p>
+                Lassen met de methode 136, ook bekend als elektrodelassen, is
+                een van de traditionele methoden om metalen te verbinden. Deze
+                methode maakt gebruik van beklede elektroden, en het lasproces
+                houdt in dat er een elektrische boog wordt onderhouden tussen de
+                elektrode en het te lassen metaal. In dit artikel zal ik in
+                detail ingaan op het proces van lassen met methode 136, de
+                werkingsprincipes ervan bespreken en praktische voorbeelden
+                geven van toepassingen.
+              </p>
+              <Image
+                src="https://i.postimg.cc/90HfbG7h/download-4.jpg"
+                alt="lasapparaat"
+              />
+              <Image
+                src="https://i.postimg.cc/CK1GjVGg/download-5.jpg"
+                alt="lasapparaat"
+              />
+              <p>
+                Werkingsprincipes van lassen met methode 136 Beklede elektrode:
+                Bij methode 136 wordt een beklede elektrode gebruikt, die
+                verschillende functies vervult. De bekleding beschermt gesmolten
+                metaal tegen de invloed van de atmosfeer, levert chemische
+                stoffen die de boog stabiliseren, en bevat legeringselementen
+                die de eigenschappen van de las verbeteren.
+              </p>{" "}
+              <Image
+                src="https://i.postimg.cc/zXzVLFSm/download-6.jpg"
+                alt="lasapparaat"
+              />
+              <Image
+                src="https://i.postimg.cc/Z5yZ0Wgt/images-6.jpg"
+                alt="lasapparaat"
+              />
+              <p>
+                Elektrische boog: De boog die wordt onderhouden tussen de
+                elektrode en het basismateriaal levert de benodigde temperatuur
+                om metalen te laten smelten en een verbinding tot stand te
+                brengen. Lasnaad: De verbinding van metalen, ook wel lasnaad
+                genoemd, ontstaat door het smelten van de elektrode en het
+                basismateriaal. De lasnaad kenmerkt zich door een hoge
+                mechanische sterkte.
+              </p>{" "}
+              <p>
+                {" "}
+                Het proces van lassen met methode 136 stap voor stap
+                Voorbereiding van het materiaal: Het te lassen metaal wordt
+                adequaat voorbereid door het te reinigen van verontreinigingen
+                en het verwijderen van geoxideerde lagen. Instelling van
+                lasparameters: Het kiezen van de juiste parameters, zoals
+                lasstroom, spanning en de snelheid waarmee de elektrode wordt
+                bewogen, is cruciaal voor het verkrijgen van een lasnaad van
+                hoge kwaliteit. Positie van de elektrode: De elektrode wordt in
+                de juiste positie gehouden ten opzichte van het basismateriaal,
+                en de boog wordt gehandhaafd op het gebied waar gelast moet
+                worden. Lassen: De elektrode nadert geleidelijk het
+                basismateriaal, waardoor de boog ontstaat. Het gesmolten metaal
+                van de elektrode en het basismateriaal combineert en vormt een
+                duurzame lasnaad. Kwaliteitscontrole: Na het voltooien van het
+                lasproces wordt de lasnaad beoordeeld op kwaliteit, worden
+                eventuele gebreken gedetecteerd en beslist de lasser over
+                eventuele correcties.
+              </p>{" "}
+              Voorbeelden van toepassingen van lassen met methode 136 Stalen
+              constructies: Lassen met methode 136 wordt veel gebruikt in de
+              bouw om structurele elementen zoals balken en kolommen te
+              verbinden. Herstel van metalen constructies: Deze methode is
+              effectief bij het repareren van metalen onderdelen, bijvoorbeeld
+              bij de renovatie van bruggen of stalen constructies.
+              Energie-industrie: Lassen met methode 136 wordt toegepast bij de
+              productie en reparatie van componenten in de energie-industrie,
+              zoals leidingen en tanks. Machinebouw: Het vervaardigen van
+              metalen machineconstructies, waar duurzaamheid en sterkte van de
+              las belangrijk zijn.
+            </Paragraph>
+          </Methoden>
+        </section>
         <SubHeader>Conclusie</SubHeader>
-        <p>
-          Lassen met de MIG/MAG-methode is een geavanceerde techniek voor het
-          verbinden van metalen die enorm populair is vanwege zijn snelheid,
-          veelzijdigheid en de duurzaamheid van de verkregen lassen. Omdat het
-          zich voortdurend ontwikkelt en aanpast aan nieuwe behoeften in de
-          industrie, kan worden verwacht dat het een essentieel onderdeel blijft
-          van de wereld van productie en constructie.
-        </p>
-      </Inleiding>{" "}
+        <Content>
+          <Paragraph>
+            Lassen met de MIG/MAG-methode is een geavanceerde techniek voor het
+            verbinden van metalen die enorm populair is vanwege zijn snelheid,
+            veelzijdigheid en de duurzaamheid van de verkregen lassen. Omdat het
+            zich voortdurend ontwikkelt en aanpast aan nieuwe behoeften in de
+            industrie, kan worden verwacht dat het een essentieel onderdeel
+            blijft van de wereld van productie en constructie.
+          </Paragraph>
+        </Content>
+      </main>
       <Footer />
     </>
   );
--- src/pages/lassen/styled.js
+++ src/pages/lassen/styled.js
@@ -9,3 +9,20 @@ export const SubHeader = styled.h2`
   text-align: center;
   margin-top: 40px;
 `;
+
+export const Methoden = styled.div`
+  background-color: #e5e5e5;
+  display: grid;
+  grid-template-columns: auto;
+  align-items: center;
+  gap: 50px;
+  margin-bottom: 50px;
+  border-radius: 25px;
+  box-shadow: 3px 3px 24px 0px rgba(193, 193, 193, 1);
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    word-break: break-all;
+  }
+`;
