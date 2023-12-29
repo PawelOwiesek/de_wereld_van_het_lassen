277b191702dd847c2b8d54b3ca922f23bc27eaf4
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-29 10:30:04 +0100

    basic styles for the lassen page

---
+++ src/pages/lassen/styled.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Header = styled.h1`
+  text-align: center;
+  color: #1f47a8;
+`;
+export const SubHeader = styled.h2`
+  grid-column: 1/4;
+  text-align: center;
+  margin-top: 40px;
+`;
