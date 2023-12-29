8ff76b31cb92ec2359a1adcaf2682a90ed6b8001
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-29 10:28:43 +0100

    add route to lassen page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./pages/main";
 import Constructiestaal from "./pages/materiaals";
 import Navbar from "./navigation";
 import { BrowserRouter as Router } from "react-router-dom";
+import Lassen from "./pages/lassen";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/mig" element={<Lassen />} />
         <Route path="/constructiestaal" element={<Constructiestaal />} />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
